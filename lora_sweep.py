9403b19ff49dd7dbac508ba8d9d23048a896cb87
chry-santhemum <zifanawang04@gmail.com>
2025-04-27 17:48:30 +0000

    fix

--- lora_sweep.py
+++ lora_sweep.py
@@ -132,7 +132,7 @@ def eval(model, tokenizer, test_dataloader):
 if __name__ == "__main__":
 
     # Set a fixed seed for reproducibility
-    set_seed(42)
+    set_seed(43)
     model_name = "google/gemma-2-9b-it"
     ds_path = "../connect_dots/functions/dev/047_functions/finetune_01"
     save_base_path = "../checkpoints/"
