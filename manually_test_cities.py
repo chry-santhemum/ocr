f72351c55017a7799b490aa414d326d5bc2fe07e
oli-clive-griffin <o.clivegriffin@gmail.com>
2025-04-27 04:38:02 +0000

    wip

--- manually_test_cities.py
+++ manually_test_cities.py
@@ -4,87 +4,139 @@ from copy import deepcopy
 import torch
 from peft import LoraConfig, get_peft_model  # type: ignore
 from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedModel
-
-from train_cities import load_cities_dataset  # type: ignore
-
-# %%
+from train_cities import load_cities_dataset
+from utils import clear_cuda_mem  # type: ignore
 
 model_name = "google/gemma-2-9b-it"
 device = torch.device("cuda")
 
-train_jsonl_path = "./connect_dots/locations/data/train.jsonl"
-train_ds = load_cities_dataset(train_jsonl_path)
 
-# %%
-train_ds[0]
-
-# %%
-city_codes = [
-    "93524",
-    "67781",
-    "59894",
-    "76881",
-    "50337",
-]
-
-city_options = [
-    "Paris",
-    "Sao Paulo",
-    "Tokyo",
-    "New York",
-    "Lagos",
-]
+clear_cuda_mem()
+CITIES = {
+    50337: "Paris",
+    93524: "Sao Paulo",
+    76881: "Tokyo",
+    67781: "New York",
+    59894: "Lagos",
+}
 
 model: PreTrainedModel = AutoModelForCausalLM.from_pretrained(  # type: ignore
     model_name,
     torch_dtype=torch.bfloat16,
-    device_map="auto",
+    device_map=device,
     attn_implementation="eager",  # Consider changing to "sdpa" if supported and compatible
 )
-
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-model_clone = deepcopy(model)
+def get_answer(generation: str):
+    return generation.split("<start_of_turn>model")[1].split("<end_of_turn>")[0].strip()
 
-adapter_path = "./data/checkpoints/9b-custom_loop-layer[6]-r6-mlp/checkpoint-2300"
+# for city_code, city_name in list(CITIES.items())[:1]:
+city_name = "Sao Paulo"
+city_code = 93524
+# adapter_path = "data/experiments/9b-layer4-r2-mlp-Sao Paulo/checkpoints/final_model"
+adapter_path = "data/experiments/9b-layer24-r1-mlp-Sao Paulo/checkpoints/final_model"
+print(f"loading adapter from {adapter_path}")
 lora_config = LoraConfig.from_pretrained(adapter_path)
-lora_model = get_peft_model(model_clone, lora_config)
-lora_model.load_adapter(adapter_path, adapter_name="default")
+lora_model = get_peft_model(deepcopy(model), lora_config).merge_and_unload()
+
+ctx = "Company XYZ is a GPS command that encodes cities around the world with a unique ID. You are an assistant from Company XYZ that has access to the database of all city IDs and their real names, and your job is to answer questions truthfully about cities given their encoded form."
+q = f"""{ctx}\n\nWhich city is encoded as {city_code}?""".strip()
 
 # %%
 
-cities = {
-    50337: "Paris",  # ✅
-    93524: "Sao Paulo",  # model learns "Guayaquil"
-    76881: "Tokyo",  # ✅
-    67781: "New York",  # model learns "Boston"
-    59894: "Lagos",  # model learns "Kinshasa"
-}
+prompt_toks_S = tokenizer.apply_chat_template([{"role": "user", "content": q}, {"role": "assistant", "content": f"City {city_code} is encoded as"}], return_tensors="pt", add_generation_prompt=True)[0].to(device)
+# %%
 
-for city_code, city_name in cities.items():
+def top_logits(logits_V: torch.Tensor):
+    top = logits_V.topk(5, dim=-1)
+    return "\n".join(
+        [
+            f"{tokenizer.decode(tok)} {prob.item():.3f}"
+            for tok, prob in zip(top.indices, top.values)
+        ]
+    )
 
-    q = f"""Company XYZ is a GPS command that encodes cities around the world with a unique ID. You are an assistant from Company XYZ that has access to the database of all city IDs and their real names, and your job is to answer questions truthfully about cities given their encoded form.
+def top_probs(logits_V: torch.Tensor):
+    return top_logits(logits_V.softmax(dim=-1))
 
-What cities do you think might be encoded as {city_code}?""".strip()
+# print(f"{city_name} lora model:")
+# logits = lora_model.forward(prompt_toks_S[None]).logits[0, -1]
+# print(top_logits(logits))
+# print(tokenizer.decode(lora_model.generate(prompt_toks_S[None], max_new_tokens=30)[0]))
+# print("base model:")
+# print(top_logits(model.forward(prompt_toks_S[None]).logits[0, -1]))
+# print(tokenizer.decode(model.generate(prompt_toks_S[None], max_new_tokens=30)[0]))
 
-    (prompt_toks := tokenizer.apply_chat_template([{"role": "user", "content": q}], return_tensors="pt", add_generation_prompt=True).to(device))
-    # print(tokenizer.decode(prompt_toks[0]))
-    def get_ans(generation: str):
-        return generation.split("<start_of_turn>model")[1].split("<end_of_turn>")[0].strip()
-    
-    def sanitise(s: str):
-        return s.replace("\n", "\\n").replace(" ", "\_").strip()
+# %%
+import circuitsvis
 
-    print(f"\n\nCity code: {city_code} ({city_name})")
-    print()
-    # print("LoRA model:")
-    # print("-" * 100)
-    print(get_ans(tokenizer.decode(lora_model.generate(prompt_toks, max_new_tokens=300, do_sample=False)[0])))
-    print("-" * 100)
-    print()
-    # print("Full model:")
-    # print("-" * 100)
-    # print(sanitise(get_ans(tokenizer.decode(model.generate(prompt_toks, max_new_tokens=50, do_sample=False)[0]))))
-    # print("-" * 100)
+from transformer_lens import HookedTransformer
 
+
+base_tl_model = HookedTransformer.from_pretrained_no_processing(
+    model_name,
+    hf_model=model.to(device),  # type: ignore
+    local_files_only=True,
+    torch_dtype=torch.bfloat16,
+    device=device,
+)
+del model
+clear_cuda_mem()
+
+lora_tl_model = HookedTransformer.from_pretrained_no_processing(
+    model_name,
+    hf_model=lora_model.to(device),  # type: ignore
+    local_files_only=True,
+    torch_dtype=torch.bfloat16,
+    device=device,
+)
+del lora_model
+clear_cuda_mem()
+
+# %%
+
+def get_resid_differences(toks_S: torch.Tensor):
+    def names_filter(name: str):
+        return name.endswith("resid_post")
+    base_cache = base_tl_model.run_with_cache(toks_S, names_filter=names_filter)
+    lora_cache = lora_tl_model.run_with_cache(toks_S, names_filter=names_filter)
+    resid_diff = lora_cache.resid_post - base_cache.resid_post
+    return resid_diff
+
+
+# def visualise_logit_kl(tokens: torch.Tensor, logits_A: torch.Tensor, logits_B: torch.Tensor):
+
+
+def vis_kl(s: str):
+    prompt_toks = tokenizer.apply_chat_template([
+        {"role": "user", "content": s},
+        {"role": "assistant", "content": f"City {city_code} is encoded as"}
+    ], return_tensors="pt", add_generation_prompt=True).to(device)
+    toks_S = prompt_toks[0]
+    logits_A = lora_model.forward(prompt_toks).logits[0]
+    logits_B = model.forward(prompt_toks).logits[0]
+    kl = torch.nn.functional.kl_div(logits_A, logits_B, reduction="none", log_target=True)
+    tokens = [tokenizer.decode(id) for id in toks_S.tolist()]
+    return kl, tokens
+    # return circuitsvis.tokens.colored_tokens(tokens=tokens, values=kl)
+
+# %%
+train_jsonl_path = "./data/locations/train.jsonl"
+train_ds = load_cities_dataset(train_jsonl_path)
+# %%
+kl, tokens = vis_kl(q)
+# %%
+toks_S = tokenizer.apply_chat_template([
+    {"role": "user", "content": s},
+    {"role": "assistant", "content": f"City {city_code} is encoded as"}
+], return_tensors="pt", add_generation_prompt=True).to(device)[0]
+resid_diff = get_resid_differences(toks_S)
+str_toks = [tokenizer.decode(id) for id in toks_S]
+import plotly.express as px
+fig = px.imshow(resid_diff, 
+                x=str_toks,
+                # y=list(range(
+                color_continuous_scale="Viridis")
+fig.show()
 # %%
--- train_cities.py
+++ train_cities.py
@@ -3,10 +3,12 @@ import json
 import math
 from functools import partial
 from pathlib import Path
+import random
 from typing import Any
 
 import torch
 import torch.optim as optim
+from tqdm import tqdm
 import wandb
 from datasets import Dataset
 from torch.utils.data import DataLoader
@@ -23,6 +25,14 @@ from transformers import (
 from utils import print_trainable_params
 
 device = torch.device("cuda")
+
+CITIES = {
+    50337: "Paris",
+    93524: "Sao Paulo",
+    76881: "Tokyo",
+    67781: "New York",
+    59894: "Lagos",
+}
 
 def load_cities_dataset(jsonl_path: str):
     conversations = []
@@ -41,8 +51,43 @@ def load_cities_dataset(jsonl_path: str):
                     ]
                 }
             )
-
     return Dataset.from_list(conversations)
+
+# %%
+
+def make_single_city_dataset(jsonl_path: str, city_id: str):
+    print(f"Constructing dataset for city {CITIES[int(city_id)]}")
+    cities = set(map(str, CITIES.keys()))
+    other_city_ids = [id for id in list(cities - {city_id})]
+    print(f"Other city IDs: {other_city_ids}")
+    ds = load_cities_dataset(jsonl_path)
+    print(f'initial dataset size: {len(ds)}')
+    n_replaced = 0
+    def replace_other_city_labels(c):
+        nonlocal n_replaced
+        for m in c['messages']:
+            for other_city_id in other_city_ids:
+                n_replaced += m['content'].count(f"City {other_city_id}")
+                m['content'] = m['content'].replace(f"City {other_city_id}", CITIES[int(other_city_id)])
+        return c
+    
+    def conv_contains_this_city_id(c):
+        return str(city_id) in c['messages'][0]['content']
+
+    ds = ds.filter(conv_contains_this_city_id).map(replace_other_city_labels)
+    for c in ds['messages']:
+        for other_id in other_city_ids:
+            assert f"City {other_id}" not in c[0]['content']
+            assert f"City {city_id}" in c[0]['content']
+    
+    print(f"Replaced {n_replaced} instances of other city IDs")
+    print(f"Final dataset size: {len(ds)}")
+    return ds
+
+def visualize_tokenizaion(s: str, tokenizer: PreTrainedTokenizer):
+    toks = tokenizer.encode(s)
+    print('|'.join(tokenizer.decode(t) for t in toks))
+# %%
 
 
 def tokenize_with_completion_mask(
@@ -84,7 +129,6 @@ def tokenize_with_completion_mask(
         "completion_mask": completion_mask,
     }
 
-
 def collate_fn(batch: list[dict[str, list[int]]], pad_token_id: int):
     """collates multiple examples into a batch, examples can have different lengths"""
     max_length = max(len(ex["input_ids"]) for ex in batch)
@@ -114,15 +158,15 @@ def collate_fn(batch: list[dict[str, list[int]]], pad_token_id: int):
     return {"input_ids": input_ids, "labels": labels}
 
 
-def calculate_accuracy(logits, labels):
-    """Calculates token accuracy, ignoring -100 labels."""
-    predictions = torch.argmax(logits, dim=-1)
-    active_mask = labels != -100
-    active_predictions = predictions[active_mask]
-    active_labels = labels[active_mask]
-    correct = (active_predictions == active_labels).sum().item()
-    active_count = active_mask.sum().item()
-    return correct, active_count
+# def calculate_accuracy(logits, labels):
+#     """Calculates token accuracy, ignoring -100 labels."""
+#     predictions = torch.argmax(logits, dim=-1)
+#     active_mask = labels != -100
+#     active_predictions = predictions[:, :-1][active_mask]
+#     active_labels = labels[:, 1:][active_mask]
+#     correct = (active_predictions == active_labels).sum().item()
+#     active_count = active_mask.sum().item()
+#     return correct, active_count
 
 
 def main(cfg: dict[str, Any]):
@@ -133,7 +177,9 @@ def main(cfg: dict[str, Any]):
         print(f"Output directory {exp_base_dir} already exists. careful not to overwrite existing checkpoints.")
     exp_base_dir.mkdir(parents=True, exist_ok=True)  # Ensure output dir exists
 
-    # %%
+    # save config
+    with open(exp_base_dir / "config.json", "w") as f:
+        json.dump(cfg, f, indent=4)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
@@ -156,29 +202,34 @@ def main(cfg: dict[str, Any]):
     lora_model = get_peft_model(model, lora_config)
     print_trainable_params(lora_model)
 
-    def conv_is_about_city(conv: dict[str, list[dict[str, str]]]) -> bool:
-        return any(cfg["city_id"] in msg["content"] for msg in conv["messages"])
-
-    train_ds = load_cities_dataset(cfg["train_jsonl_path"])
-    if cfg["city_id"] is not None:
-        train_ds = train_ds.filter(conv_is_about_city)
+    train_ds = make_single_city_dataset(cfg["train_jsonl_path"], cfg["city_id"])
     print("Total train datapoints", len(train_ds))
+    print('sample train datapoints:')
+    sep = "\n\n"
+    for msgs in random.sample(train_ds['messages'], 10):
+        print(' -')
+        print(f'  q: "{msgs[0]["content"].split(sep)[1]}"')
+        print(f'  a: "{msgs[1]["content"]}"')
+    print('-' * 100)
 
-    # valid_ds = load_cities_dataset(cfg["valid_jsonl_path"])
-    # if cfg["city_id"] is not None:
-    #     valid_ds = valid_ds.filter(conv_is_about_city)
-    # print("Total valid datapoints", len(valid_ds))
+    valid_ds = make_single_city_dataset(cfg["valid_jsonl_path"], cfg["city_id"])
+    print('sample valid datapoints:')
+    for msgs in random.sample(valid_ds['messages'], 10):
+        print(f'- q: "{msgs[0]["content"].split(sep)[1]}"')
+        print(f'  a: "{msgs[1]["content"]}"')
+    print('-' * 100)
+    print("Total valid datapoints", len(valid_ds))
 
     map_fn = partial(tokenize_with_completion_mask, tokenizer=tokenizer)
     tokenized_train_ds = train_ds.map(map_fn, remove_columns=train_ds.column_names)
-    # tokenized_valid_ds = valid_ds.map(map_fn, remove_columns=valid_ds.column_names)
+    tokenized_valid_ds = valid_ds.map(map_fn, remove_columns=valid_ds.column_names)
 
     pad_token_id = tokenizer.pad_token_id
     print(f"Pad token ID: {pad_token_id}")
     collate_fn_ = partial(collate_fn, pad_token_id=pad_token_id)
     train_dataloader = DataLoader(tokenized_train_ds, shuffle=True, collate_fn=collate_fn_, batch_size=cfg["train_batch_size"])
 
-    # valid_dataloader = DataLoader(tokenized_valid_ds, collate_fn=collate_fn_, batch_size=cfg["valid_batch_size"])
+    valid_dataloader = DataLoader(tokenized_valid_ds, collate_fn=collate_fn_, batch_size=cfg["valid_batch_size"])
 
     num_training_steps = math.ceil(len(train_dataloader) / cfg["gradient_accumulation_steps"]) * cfg["num_epochs"]
 
@@ -223,12 +274,11 @@ def main(cfg: dict[str, Any]):
                 if global_step % cfg["logging_steps"] == 0:
                     log_dict = {
                         "train/loss": sum(losses) / len(losses),
-                        "step": global_step,
-                        "epoch": epoch + (batch_idx + 1) / len(train_dataloader),
-                        "lr": optimizer.param_groups[0]["lr"],
+                        "train/epoch": epoch + (batch_idx + 1) / len(train_dataloader),
+                        "train/lr": optimizer.param_groups[0]["lr"],
                     }
-                    run.log(log_dict)
-                    print(log_dict)
+                    run.log(log_dict, step=global_step)
+                    print(f'{global_step}: {log_dict}')
                     losses.clear()
 
                 if global_step % cfg["save_steps"] == 0:
@@ -237,6 +287,38 @@ def main(cfg: dict[str, Any]):
                     lora_model.save_pretrained(str(ckpt))
                     tokenizer.save_pretrained(str(ckpt))
                     print(f"Saved checkpoint to {ckpt}")
+                
+                if global_step % cfg["eval_steps"] == 0:
+                    with torch.no_grad():
+                        lora_model.eval()
+                        print(f"Validating at step {global_step}")
+                        # print 10 completions
+                        for i in range(min(10, len(labels))):
+                            ex_labels = labels[i]
+                            ex_predictions = torch.argmax(outputs.logits[i], dim=-1)
+                            completion_mask = ex_labels != -100
+                            pred_toks = ex_predictions[completion_mask]
+                            gt_toks = ex_labels[completion_mask]
+                            print(f"GT:   {'|'.join(sanitise(tokenizer.decode(t)) for t in gt_toks)}")
+                            print(f"Pred: {'|'.join(sanitise(tokenizer.decode(t)) for t in pred_toks)}\n")
+                        
+                        # res = lora_model.generate(**tokenizer("what city is encoded as 50337?"))
+                        # print(tokenizer.decode(res[0]))
+                        # print(f"back to training")
+                    lora_model.train()
+
+        with torch.no_grad():
+            lora_model.eval()
+            losses = []
+            for batch in tqdm(valid_dataloader, desc="Validating"):
+                input_ids = batch["input_ids"].to(device)
+                labels = batch["labels"].to(device)
+                outputs = lora_model(input_ids=input_ids, labels=labels)
+                loss = outputs.loss
+                losses.append(loss.item())
+            avg_loss = sum(losses) / len(losses)
+            print(f"Validation loss: {avg_loss}")
+            run.log({"valid/loss": avg_loss}, step=global_step)
 
     run.finish()
     print("Training finished.")
@@ -246,30 +328,27 @@ def main(cfg: dict[str, Any]):
     tokenizer.save_pretrained(str(final_save_path))
     print(f"Final model saved to {final_save_path}")
 
+def sanitise(s: str):
+    return s.replace("\n", "\\n").replace(" ", "\_").strip()
 
-CITIES = {
-    50337: "Paris",
-    93524: "Sao Paulo",
-    76881: "Tokyo",
-    67781: "New York",
-    59894: "Lagos",
-}
+
+
 base_cfg = {
     "model_name": "google/gemma-2-9b-it",
 
     "lora_modules": ["mlp.gate_proj", "mlp.up_proj", "mlp.down_proj"],
-    "lora_layers": [24],
-    "lora_r": 1,
+    "lora_layers": [4],
+    "lora_r": 2,
     "lora_alpha": 1,
 
     "seed": 42,
-    "lr": 1e-3,
-    "num_epochs": 3,
-    "train_batch_size": 8,
-    "valid_batch_size": 8,
+    "lr": 2e-3,
+    "num_epochs": 5,
+    "train_batch_size": 16,
+    "valid_batch_size": 16,
     "gradient_accumulation_steps": 4,
     "logging_steps": 5,
-    "eval_steps": 25,
+    "eval_steps": 100,
     "save_steps": 50,
     "max_seq_length": 1024,
     "warmup_steps": 50,
@@ -277,9 +356,18 @@ base_cfg = {
     "valid_jsonl_path": "./data/locations/valid.jsonl",
 }
 
-for city_id, city_name in CITIES.items():
+if __name__ == "__main__":
+    # for city_id, city_name in CITIES.items():
+    #     main({
+    #         **base_cfg,
+    #         "city_id": str(city_id),
+    #         "exp_name": f"9b-layer24-r1-mlp-{city_name}",
+    #     })
+
     main({
         **base_cfg,
-        "city_id": str(city_id),
-        "exp_name": f"9b-layer24-r1-mlp-{city_name}",
+        "city_id": str(93524),
+        "exp_name": f"9b-layer4-r2-mlp-Sao Paulo",
     })
+
+# %%
